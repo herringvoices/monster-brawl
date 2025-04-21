bb55059bd3a1e9f90aa54d69ab7affb73850cb5d
herringvoices <nalherring@gmail.com>
2025-04-21 13:27:49 -0500

    chore: initial scaffold with React‑TS, Bootstrap, SCSS and folder structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="p-2">
+      <h1>Monster Brawl</h1>
+    </Container>
+  );
+}
+
+export default App;
